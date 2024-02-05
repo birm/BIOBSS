32f4808c5fba8c9ab1cfabcb900cd98e47447b82
Birm <birm@rbirm.us>
2024-02-05 13:09:56 -0500

    [ENH] Optimization: precalculate values sum, substantial speedup

--- biobss/common/signal_entropy.py
+++ biobss/common/signal_entropy.py
@@ -20,8 +20,9 @@ def calculate_shannon_entropy(sig: ArrayLike, base: int = 2) -> float:
     """
     # count values in sig
     value_counts = collections.Counter([value for value in sig])
+    value_count_sums = sum(value_counts.values())
     # calculate probabilities
-    pk = [x / sum(value_counts.values()) for x in value_counts.values()]
+    pk = [x / value_count_sums for x in value_counts.values()]
     # use scipy.stats to calculate entropy
     entropy_value = stats.entropy(pk=pk, base=base)
 
